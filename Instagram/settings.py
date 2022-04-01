fa936784e519ac827966479f609eb2f913fcb329
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-01 22:20:27 +0300

    add crispy_forms

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'photo',
+    'photo.apps.PhotoConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
