c4572e0c8ef55dada173ebc95ade0dd2ae9792b3
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-02 18:33:10 +0300

    add a new template for user to edit

--- photo/urls.py
+++ photo/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome,name='welcome'),
+    path('photo/article$', views.new_article, name='new-post'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, NewImageForm
 from .models import Image, Profile, Comment, Likes, Follow
 
 # Create your views here.
@@ -25,4 +25,17 @@ def register(response):
         form = RegisterForm()
     return render(response, 'register/register.html', {'form': form})
 
-     
+
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = current_user
+            article.save()
+        return redirect('home')
+
+    else:
+        form = NewImageForm()
+    return render(request, 'new_post.html', {"form": form})     
