d0c74b1353f85e6c889a44236b8f9591827a03f3
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-02 13:23:44 +0300

    image methods

--- photo/models.py
+++ photo/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import cloudinary
 from cloudinary.models import CloudinaryField
 
 class Image(models.Model):
@@ -9,13 +10,43 @@ class Image(models.Model):
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    comments= models.ManyToManyField('Comment', blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, new_caption):
+        self.caption = new_caption
+        self.save()
+    
+
 
 class Profile(models.Model):
     profile_pic = CloudinaryField('image')
     bio = models.TextField(max_length=500, blank=True)
+
+
 class Comment(models.Model):
     comment = models.TextField(max_length=500)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     image = models.ForeignKey('Image', on_delete=models.CASCADE)
-       
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+
+
+class Likes(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.ForeignKey('Image', on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    dislike = models.BooleanField(default=False)       
+
+
+class Follow(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    follower = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='follower')
+    following = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='following')   
