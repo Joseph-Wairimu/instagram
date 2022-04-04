73c69cb9c83c9ce79ebfcc2358210a026ed14bf4
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-04 10:19:38 +0300

    email username

--- photo/views.py
+++ photo/views.py
@@ -16,6 +16,7 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
+            
            
             username = form.cleaned_data['username']
             email = form.cleaned_data['email']
